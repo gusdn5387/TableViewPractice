1ff922c4d6c56a476a0b72d60fa0bfec3767baea
gusdn5387 <gusdn5387@naver.com>
2022-02-03 13:30:22 +0900

    Xcode Project 생성

---
+++ TableViewPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewPractice
+//
+//  Created by Byapps on 2022/02/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
